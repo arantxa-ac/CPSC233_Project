00e04ab8d67ad414ae891b9d2dbe51366bd39a68
Yuqi-rrr <60750020+Yuqi-rrr@users.noreply.github.com>
2020-02-26 10:26:24 -0700

    Update Obstacles.java
    comments added

--- Obstacles.java
+++ Obstacles.java
@@ -1,13 +1,17 @@
 import java.util.Random;
 public class Obstacles {
+	//defalt Obstacles and rate
 	private static String Obstacle = "|";
 	private static int rate = 5;
 	
+	//constructors to change defalt values
 	public Obstacles(int rate, String Obstacle) {
 		this.rate = rate;
 		this.Obstacle = Obstacle;
 	}
 	
+	//Generate random Obstacles by the rate
+	//will retrun either empty or an Obstacles
 	public static String getNewObstacles() {
 		int Ob;
 		Random rand = new Random();
