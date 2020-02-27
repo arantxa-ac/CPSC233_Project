615fc5145a41703d1305902d09863ea7d2e50630
Bhoang01 <49581189+Bhoang01@users.noreply.github.com>
2020-02-26 23:17:42 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,8 +1,7 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Game {
+public class Demo1Game {
 	//First import needed objects
 	static Player runner = new Player();	
 	static Scanner userInput = new Scanner(System.in);
@@ -24,6 +23,7 @@ public class Game {
 
 		// it runs until the player doesnt jump when the index of obstacles are(aka where the player is at)
 		do {
+			System.out.println();
 			System.out.println("Click 'j' to jump!");
 			
 			//Removes the character closest to the player and adds a new one at the end
@@ -36,7 +36,6 @@ public class Game {
 			System.out.println();
 			jump = userInput.next();
 			runner.setInput(jump);
-			System.out.println();
 			
 			if(runner.getJumpInput()) {
 				System.out.println("Player jumped!");
@@ -44,14 +43,16 @@ public class Game {
 			
 			// If a player 'jumps' over the obstacle right in front of it, it vanishes on the screen and 
 			// a new character generated at the end of the list
-			if(textDemo.get(1) == "|" && runner.getJumpInput()) {
+			if(runner.getJumpInput() && textDemo.get(1) == "|") {
 				textDemo.remove(1);
 				textDemo.add(Obstacle.getNewObstacles());
 				System.out.println("The obstacle in front of you vanished!");
 			}
+			else if(!runner.getJumpInput() && textDemo.get(1) == "|")
+				textDemo.add(0, "|");
 			// This needs to be an else, otherwise the characters at both index 1 and 2 will be deleted if 
 			// index 1 = '|'
-			else {
+			else{
 				textDemo.remove(1);
 				textDemo.add(Obstacle.getNewObstacles());
 			}
