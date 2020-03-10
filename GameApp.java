59deab4b9163cb95966bb0ada792befca7652f72
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-03-10 14:38:05 -0600

    Update GameApp.java

--- GameApp.java
+++ GameApp.java
@@ -1,12 +1,13 @@
-package application;
-	
+import java.util.Collection;
 
 import javafx.application.*;
 import javafx.event.EventHandler;
 import javafx.stage.*;
+import javafx.util.Duration;
 import javafx.scene.*;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.input.*;
 import javafx.scene.input.KeyCode;
@@ -14,39 +15,38 @@ import javafx.scene.input.KeyCode;
 
 public class GameApp extends Application{
 	@Override
+	//Override the start method.
 	public void start(Stage primaryStage) {
+		//Try-except block to print to console in case of exception.
 		try {
+			
+			//Create root pane.
 			Pane root = new Pane();
 			
-			//Prints actions, only for practice----------------------------
-			BorderPane labelPane = new BorderPane();
-			root.getChildren().add(labelPane);
-			Label practiceLabel = new Label("start game");
-			labelPane.setTop(practiceLabel);
-			//-----------------------------------------------
+			//Create new layer and add a player to it
+			Pane playerPane = new Pane();
+			Image image = new Image("trexPlayer.png");
+			Player player = new Player(playerPane,image,0,100);
+			root.getChildren().add(playerPane);			
 			
-			BorderPane playerPane = new BorderPane();
-			root.getChildren().add(playerPane);
-			playerPane.setTop(createPlayer());
-			
-			
+			//Create a scene.
 			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());			
 			
+			//Event handler. If up arrow key is pressed then the player will jump.
 			scene.setOnKeyPressed(new EventHandler<KeyEvent>()
 			{
 				@Override
 				public void handle(KeyEvent event)
 				{
 					if (event.getCode() == KeyCode.UP)
-						//Prints action only for practice ---------------------
-						practiceLabel.setText("Jump");
-						//---------------------------------------------------------
-						//Later this will call player.jump();
+						player.jump();
 				}
 			});
-					
+				
+			//Give the stage a title.
 			primaryStage.setTitle("T-Rex Run");
+			//Set the scene on primary stage and show stage.
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
@@ -55,12 +55,7 @@ public class GameApp extends Application{
 		}
 	}
 	
-	public Group createPlayer()
-	{
-		Group playerGroup = new Group();
-		return playerGroup;
-	}
-	
+	//Start application by calling launch.
 	public static void main(String[] args) {
 		launch(args);
 		
