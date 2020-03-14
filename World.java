873b6b07ebd39644ca21b73a8b5d8c47d18478a1
hasan-na <60748908+hasan-na@users.noreply.github.com>
2020-03-13 23:40:05 -0400

    Update World.java

--- World.java
+++ World.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 public class World extends GameObject {
 
 	private static ArrayList<GameObject> game;
-	
+	// Add the game objects into the world and print it
 	public World() {
 		game.add(Obstacle.generate());
 		for (int n = 0; n < game.size(); n++) {
@@ -11,16 +11,19 @@ public class World extends GameObject {
 		System.out.print("\n");
 	}
 	
-	
-	public void update() {
+	// Update the world every frame
+	public static void update() {
+		// Create a new ArrayList
 		ArrayList<GameObject> preWorld = new ArrayList<GameObject>();
+		// Add each object from the game ArrayList into the new ArrayList
 		for (int n = 0; n < game.size(); n++) {
 			preWorld.add(game.get(n));
 		}
-		
+		// Replace the object in the last index to update the world
 		for (int i = 0; i < (game.size() - 1) ; i++) {
 			game.set(i, preWorld.get(i+1));
 		}
-		game.set((game.size() - 1) , Obstacle.generate());
+		game.set(game.size() , Obstacle.generate());
 	}
 }
+
