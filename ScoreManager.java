7b5a8dd77b08d34b5f089d1d1cd574dbfbaf053f
hasan-na <60748908+hasan-na@users.noreply.github.com>
2020-03-13 23:37:02 -0400

    Update ScoreManager.java

--- ScoreManager.java
+++ ScoreManager.java
@@ -4,29 +4,32 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class ScoreManager {
-	private int millisecondsPassed = 0;
+	private int tenthsecondsPassed = 0;
 	private int score = 0;
-	
+	// Add a timer for the score to take place
 	Timer scoreCalculator = new Timer();
 	TimerTask task = new TimerTask() {
+	// Increment score for every tenth of a second and print it
 		public void run() {
-			millisecondsPassed++;
-			score = millisecondsPassed;
+			tenthsecondsPassed++;
+			score = tenthsecondsPassed;
 			System.out.println("Score is: " + score);
 		}
 	};
 	
+	// Score will increment every 10th of a second
 	public void start() {
 		scoreCalculator.scheduleAtFixedRate(task, 100, 100);
 	}
-
+	// Where the score label will be placed in the GUI version
 	public void scoreLabel(int score) {
 		Label scoreLabel = new Label();
 		scoreLabel.setFont(new Font("arial", Font.PLAIN, 14));
 	}
-	
+	// Start of the score
 	public static void main(String[] args) {
 		ScoreManager currenttime = new ScoreManager();
 		currenttime.start();
 	}
 }
+
