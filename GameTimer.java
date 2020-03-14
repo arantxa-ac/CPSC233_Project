b55b3fe5702e84299ba42fc8a317f7ceb32f361b
hasan-na <60748908+hasan-na@users.noreply.github.com>
2020-03-13 23:38:54 -0400

    Update GameTimer.java

--- GameTimer.java
+++ GameTimer.java
@@ -7,23 +7,27 @@ import java.util.TimerTask;
 public class GameTimer {
 	private int secondsPassed = 0;
 
+	// Add a timer for each second
 	Timer timer = new Timer();
 	TimerTask task = new TimerTask() {
+		// Increment for each second that passed and print it to console
 		public void run() {
 			secondsPassed++;
 			System.out.println("Time: " + secondsPassed);
 		}
 	};
-	
+	// Start the increment for each second that passes
 	public void start() {
 		timer.scheduleAtFixedRate(task, 1000, 1000);
 	}
-
+	
+	// Where the label of the timer will be for the GUI version
 	public void gameTimerLabel() {
 		Label timerLabel = new Label();
 		timerLabel.setFont(new Font("arial", Font.PLAIN, 14));
 	}
 	
+	// Start of the timer
 	public static void main(String[] args) {
 		GameTimer currenttime = new GameTimer();
 		currenttime.start();
