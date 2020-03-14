2f5bbe576e696ca11652be1ec3d5b62728ae820d
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-03-13 21:31:08 -0600

    Update GUIGame.java

--- GUIGame.java
+++ GUIGame.java
@@ -1,19 +1,17 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
 
 public class GUIGame extends Application {
 	
 	static Scene scene;
-	Stage primaryStage;
 	Pane root;
-	World world = new World();
-	PlayerGUI player = new PlayerGUI();
+	static World world = new World();
+	static PlayerGUI player = new PlayerGUI();
 	Obstacle obstacle = new Obstacle();
-	Ground ground = new Ground();
+//	Ground ground = new Ground();
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -23,7 +21,7 @@ public class GUIGame extends Application {
 			//Add player, ground and obstacle layers.
 			root.getChildren().add(player.getLayer(player));
 			root.getChildren().add(obstacle.getLayer());
-			root.getChildren().add(ground.getLayer());
+//			root.getChildren().add(ground.getLayer());
 			
 			scene = new Scene(root,DataProvider.getWINDOW_WIDTH(),DataProvider.getWINDOW_HEIGHT());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
