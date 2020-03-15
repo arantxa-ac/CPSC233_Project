6630f3695470d5767abd2dc3f2eeb3a7062d2557
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-03-14 18:52:04 -0600

    Update GUIGame.java

--- GUIGame.java
+++ GUIGame.java
@@ -1,31 +1,36 @@
+import javax.management.ObjectName;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
+
 
 
 public class GUIGame extends Application {
 	
 	static Scene scene;
-	Pane root;
-	static World world = new World();
-	static PlayerGUI player = new PlayerGUI();
-	Obstacle obstacle = new Obstacle();
-//	Ground ground = new Ground();
 	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) {		
 		try {
-			root = new Pane();
-			
-			//Add player, ground and obstacle layers.
-			root.getChildren().add(player.getLayer(player));
-			root.getChildren().add(obstacle.getLayer());
-//			root.getChildren().add(ground.getLayer());
-			
+			 Pane root = new Pane();
+			 
+			 Pane layers = new Pane();
+			 
+//			 World world = new World();
+			 root.getChildren().add(renderGUI(layers));
+			 
 			scene = new Scene(root,DataProvider.getWINDOW_WIDTH(),DataProvider.getWINDOW_HEIGHT());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		//STAGE
+			//Give the stage a title.
+			primaryStage.setTitle("T-Rex Run");
+			//Set the scene on primary stage and show stage.
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} 
 		catch(Exception e) {
@@ -33,24 +38,28 @@ public class GUIGame extends Application {
 		}
 	}
 	
-//	public static void renderGUI()
-//	{
-//		
-//	}
+	public Pane renderGUI(Pane toAdd)
+	{
+//		Ground ground = new Ground();
+//		toAdd.getChildren().add(ground.getLayer());
+		PlayerGUI player = new PlayerGUI();
+		toAdd.getChildren().add(player.getLayer());
+//		world.getGame().forEach((object)-> toAdd.getChildren().add(object.getLayer()));
+		return toAdd;
+	}
 	
 	public static void main(String[] args) {
-		launch();
-		while(!player.checkCollision())
-		{
-			player.processInput(getScene());
-			world.update();
-//			renderGUI();
-		}
+		launch();		
+//		while(!player.checkCollision())
+//		{
+//			player.processInput(getScene());
+//			world.update();
+//			renderGUI(world);
+//		}
 	}
 
 	public static Scene getScene() 
 	{
 		return scene;
 	}
-
 }
