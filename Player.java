0ed719356373b1dab6b55d67980bc5f980822b89
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-03-14 18:05:36 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,40 +1,70 @@
-import java.awt.Rectangle;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
-public class Player extends GameObject 
-
+public class PlayerGUI extends Player
 {
-
-	private double initalVelocity;
-	private double gravity;
 	
-	 World obstacleList = new World();
+	int HITBOXSIZE = 95;
+	int X = 0;
+	int Y = 200;
+	Image image = new Image(getClass().getResourceAsStream(DataProvider.getPLAYER_RUN_IMAGE()));
+	ImageView imageView = new ImageView(image);
+	Duration duration = Duration.millis(DataProvider.getPLAYER_SPEED());
+	int count = DataProvider.getPLAYER_RUN_SETTINGS()[0];
+	int columns = DataProvider.getPLAYER_RUN_SETTINGS()[1];
+	int offsetX = DataProvider.getPLAYER_RUN_SETTINGS()[2];
+	int offsetY = DataProvider.getPLAYER_RUN_SETTINGS()[3];
+	int width = DataProvider.getPLAYER_RUN_SETTINGS()[4];
+	int height = DataProvider.getPLAYER_RUN_SETTINGS()[5];
+	Sprite playerSprite= new Sprite(HITBOXSIZE,X,Y);
 	
-	public void Jump()
+	public void processInput(Scene scene)
 	{
-		initalVelocity = DataProvider.getINITAL_VELOCITY();
-		gravity = DataProvider.getACCELERATION();
-		while (initalVelocity > (-1*initalVelocity))
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>()
 		{
-			initalVelocity -= gravity;
-		}
-		
-	}
-	
-	public boolean checkCollision()
-	{
-		for (GameObject g: obstacleList.getWorldList())
-		{
-			if (g.getClass() == Obstacle)
+			@Override
+			public void handle(KeyEvent event)
 			{
-				if(g.getHitbox().intersects(getWorldList.get(0).hitbox()
-						{
-						return true;
-						}
+				if (event.getCode() == KeyCode.UP)
+					getPlayerGUI().Jump();
 			}
-				
-		}
-		return false;
-				
+		});
 	}
 	
+	public Pane getLayer()
+	{
+		Pane playerPane = new Pane();
+		playerSprite.hitbox();
+		SpriteAnimation animation = new SpriteAnimation(imageView,duration,count,columns,offsetX,offsetY,width,height);
+		imageView.setX(X);
+		imageView.setY(Y);
+		playerPane.getChildren().add(imageView);
+		return playerPane;
+	}
+
+	public void animate(Image image, Duration duration, int count, int columns, int offset_X, int offset_Y, int width, int height) {
+		ImageView imageView = new ImageView(image);
+		imageView.setViewport(new Rectangle2D(offset_X, offset_Y, width, height));
+		SpriteAnimation animation = new SpriteAnimation(imageView,duration, count, columns, offset_X, offset_Y, width, height);
+		 animation.setCycleCount(animation.INDEFINITE);
+	     animation.play();
+	}
+	
+	public PlayerGUI getPlayerGUI()
+	{
+		return this;
+	}
+
+	@Override
+	public Image getImage() {
+		return image;
+	}
 }
