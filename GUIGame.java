7460e85f9e108d3270e07edcbbdc3a0a917a1cf8
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-03-14 23:58:30 -0600

    Update GUIGame.java

--- GUIGame.java
+++ GUIGame.java
@@ -1,11 +1,17 @@
+import java.awt.Label;
+import java.awt.event.KeyListener;
+
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 
 
 
-public class GUIGame extends Application {
+public class GUIGame extends Application implements KeyListener {
 	
 	static Scene scene;
 	
@@ -13,14 +19,35 @@ public class GUIGame extends Application {
 	public void start(Stage primaryStage) {		
 		try {
 			 Pane root = new Pane();
-			 
 			 Pane layers = new Pane();
-			 
+			 PlayerGUI player = new PlayerGUI();
 //			 World world = new World();
-			 root.getChildren().add(renderGUI(layers));
 			 
+		 
+//				while(!player.checkCollision())
+//				{
+//					world.update();
+					root.getChildren().add(renderGUI(layers,player));
+//				}
+			 
+					
+		//SCENE
 			scene = new Scene(root,DataProvider.getWINDOW_WIDTH(),DataProvider.getWINDOW_HEIGHT());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+		//EVENT HANDLER
+			 scene.setOnKeyPressed(new EventHandler<KeyEvent>()
+				{
+					@Override
+					public void handle(KeyEvent event)
+					{
+						if (event.getCode() == KeyCode.UP)
+						{
+							player.Jump();
+							
+						}
+					}
+				});
 		//STAGE
 			//Give the stage a title.
 			primaryStage.setTitle("T-Rex Run");
@@ -34,11 +61,10 @@ public class GUIGame extends Application {
 		}
 	}
 	
-	public Pane renderGUI(Pane toAdd)
+	public Pane renderGUI(Pane toAdd, PlayerGUI player)
 	{
 		GroundGUI ground = new GroundGUI();
 		toAdd.getChildren().add(ground.getLayer());
-		PlayerGUI player = new PlayerGUI();
 		toAdd.getChildren().add(player.getLayer());
 //		world.getGame().forEach((object)-> toAdd.getChildren().add(object.getLayer()));
 		return toAdd;
@@ -46,16 +72,28 @@ public class GUIGame extends Application {
 	
 	public static void main(String[] args) {
 		launch();		
-//		while(!player.checkCollision())
-//		{
-//			player.processInput(getScene());
-//			world.update();
-//			renderGUI(world);
-//		}
 	}
 
 	public static Scene getScene() 
 	{
 		return scene;
+	}
+
+	@Override
+	public void keyTyped(java.awt.event.KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(java.awt.event.KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(java.awt.event.KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
