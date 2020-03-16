799e95447c4760f61187b2823dee8863f354cbd6
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-03-16 00:42:24 -0600

    Update ObstacleGUI.java

--- ObstacleGUI.java
+++ ObstacleGUI.java
@@ -1,26 +1,32 @@
 import java.util.Random;
-import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 public class ObstacleGUI extends Obstacle{
 
+	//Instance Variables for ObstacleGUI
 	int HITBOXSIZE = 95;
-	int X = DataProvider.getWINDOW_WIDTH();
+	int X = 300;
 	int Y = 200;
-	Image Obstacle_image = new Image(getClass().getResourceAsStream(DataProvider.getSINGLE_CACTUS()));
-	ImageView imageView = new ImageView(Obstacle_image);
+	Image Obstacle_image;
+	ImageView imageView = new ImageView();
+	int count;
+	int columns;
+	int offsetX;
+	int offsetY;
+	int width;
+	int height;
+	Duration duration;
 	Sprite obstacleSprite= new Sprite(HITBOXSIZE,X,Y);
+	private static int rate = 1;
 	
 
-	
+	//GET LAYER METHOD
+		//Returns:
+		//	-Pane that contains imageView with object in the specified coordinates.
+	@Override
 	public Pane getLayer()
 	{
 		Pane obstaclePane = new Pane();
@@ -31,6 +37,21 @@ public class ObstacleGUI extends Obstacle{
 		return obstaclePane;
 	}
 	
+	//GENERATE METHOD
+	//Returns:
+	//	-ObstacleGUI that is generated ramdomly.
+	public ObstacleGUI generate()
+	{
+		Random rand = new Random();
+		int generate = rand.nextInt(rate);
+		if(generate == 0)
+			return new ObstacleGUI();
+		return null;
+	}
+	
+	//GETTER FOR OBSTACLE_GUI
+		//Returns:
+		//	-ObstacleGUI
 	public ObstacleGUI getObstacleGUI()
 	{
 		return this;
