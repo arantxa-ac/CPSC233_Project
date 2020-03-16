9029b67e35a303f330495d2e9057cb05ff58e598
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-03-16 00:46:10 -0600

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -1,11 +1,17 @@
 import java.awt.Rectangle;
 import java.util.Random;
 
+import javafx.scene.layout.Pane;
+
 public class Obstacle extends GameObject {
 	
+	//Instance variables for Obstacle.
 	private int rate;
 	private Rectangle hitbox;
 	
+	//GENERATE METHOD
+		//Returns:
+		//	-Obstacle that is generated randomly.
 	public Obstacle generate()
 	{
 		Random rand = new Random();
@@ -14,6 +20,17 @@ public class Obstacle extends GameObject {
 			return new Obstacle();
 		else
 			return null;
+	}
+
+	//Override of getLayer() and getSprite() Methods from abstract parent class Game Object
+	@Override
+	public Pane getLayer() {
+		return null;
+	}
+
+	@Override
+	public Sprite getSprite() {
+		return null;
 	}
 
 }
