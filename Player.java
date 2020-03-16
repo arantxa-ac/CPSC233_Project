8e1a0d7b1dd01191c06e4e97cf39e1a86a462907
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-03-16 00:20:04 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,15 +1,12 @@
-import java.awt.Rectangle;
+import javafx.scene.layout.Pane;
 
-public abstract class Player extends GameObject 
-
+public class Player extends GameObject 
 {
-
-//	World world = new World();
-
-	public void Jump()
+	
+	//JUMP METHOD
+	//Makes player jump.
+	public void jump()
 	{
-		//int initialVelocity = (int) DataProvider.getINITIAL_VELOCITY();
-		//double gravity = DataProvider.getACCELERATION();
 		int maxHeight = 0;
 		int originalHeight = 120;
 		int currentHeight = getY();
@@ -21,18 +18,38 @@ public abstract class Player extends GameObject
 			setY(currentHeight ++);
 			}
 		}
-	public boolean checkCollision()
+	
+	//CHECK COLLISION METHOD
+	//Parameters:
+	// -World in which to check for a collision between the player and the obstacles.
+	//Returns:
+	//	-Boolean: True if there has been a collision between the player and the obstacles, false otherwise.
+	public boolean checkCollision(World world)
 	{
-//		for (GameObject g: world.getGame())
-//		{
-//			if (g instanceof Obstacle)
-//			{
-//				if(g.getSprite().hitbox().intersects(world.getGame().get(0).hitbox()))
-//						{
-//						return true;
-//						}
-//			}
-//
-//		}
-		return false;
+		for (GameObject g: world.getGame())
+		{
+			if (g instanceof Obstacle && g != null)
+			{
+				if(g.getSprite().hitbox().intersects(world.getGame().get(0).getSprite().hitbox()))
+						{
+						return true;
+						}
+			}
 
+		}
+		return false;
+		
+	}
+
+	
+	//Override of getLayer() and getSprite() Methods from abstract parent class Game Object
+	@Override
+	public Pane getLayer() {
+		return null;
+	}
+
+	@Override
+	public Sprite getSprite() {
+		return null;
+	}
+}
