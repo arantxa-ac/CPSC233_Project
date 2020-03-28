ad9f6fb8c56ffb9618e85443e757793bb65de3d7
Bhoang01 <49581189+Bhoang01@users.noreply.github.com>
2020-03-28 16:24:51 -0600

    Update GameObject.java

--- GameObject.java
+++ GameObject.java
@@ -1,40 +1,53 @@
 import javafx.scene.layout.Pane;
 
+/**
+ * 
+ * The initial class of the game, such that every component within the game extends it
+ *
+ */
 public abstract class GameObject {
 	private int x;
 	private int y;
 
-	//GETTER FOR X COORDINATE
-	//Returns:
-	//	-X coordinate of Game object.
+	/**
+	 * Getter for X-Coordinate 
+	 * @return x position as an int on the Cartesian plane
+	 */
 	public int getX() {
 		return x;
 	}
 	
-	//SETTER FOR X COORDINATE
-	//Parameters:
-	//	-New x coordinate for the Game object.
+	/**SETTER FOR X COORDINATE
+	 * @param int x which represents a new coordinate for the game object
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 	
-	//GETTER FOR Y COORDINATE
-		//Returns:
-		//	-Y coordinate of Game object.
+	/**
+	 * Getter for Y-Coordinate 
+	 * @return y position as an int on the Cartesian plane
+	 */
 	public int getY() {
 		return y;
 	}
 	
-	//SETTER FOR Y COORDINATE
-		//Parameters:
-		//	-New y coordinate for the Game object.
+	/**SETTER FOR Y COORDINATE
+	 * @param int y which represents a new coordinate for the game object
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 	
-	//ABSTRACT GETTER FOR THE LAYER IN WHICH GAME OBJECT IS CONTAINED	
+	/**
+	 * ABSTRACT GETTER FOR THE LAYER IN WHICH GAME OBJECT IS CONTAINED	
+	 *
+	 */
 	public abstract Pane getLayer();
 	
-	//ABSTRACT GETTER FOR THE SPRITE OF THE GAME OBJECT
+	/**
+	 * ABSTRACT GETTER FOR THE SPRITE OF THE GAME OBJECT
+	 *
+	 */
 	public abstract Sprite getSprite();
 }
