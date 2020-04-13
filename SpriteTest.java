e32752f84d939725abd02d820b972e8a72e50675
hasan-na <60748908+hasan-na@users.noreply.github.com>
2020-04-13 18:25:51 -0400

    Update SpriteTest.java

--- SpriteTest.java
+++ SpriteTest.java
@@ -4,30 +4,42 @@ import javafx.scene.shape.*;
 
 
 import org.junit.jupiter.api.Test;
-
+/**
+ * 
+ * A test class for the Sprite class
+ *
+ */
 public class SpriteTest {
-	
+	/**
+	 * Testing to see if the height of the hitbox is accurate
+	 */
 	@Test
 	public void test_getHitboxHeight() {
 		Sprite c = new Sprite(10,20,30);
 		Rectangle hitbox = new Rectangle(15, 35, 10 , 10);
 		assertEquals("Testing Hitbox height" , hitbox.getHeight(), c.getHitbox().getHeight(), 0.001);
 	}
-	
+	/**
+	 * Testing to see if the width of the hitbox is accurate
+	 */
 	@Test
 	public void test_getHitboxWidth() {
 		Sprite c = new Sprite(10,20,30);
 		Rectangle hitbox = new Rectangle(15, 35, 10 , 10);
 		assertEquals("Testing Hitbox height" , hitbox.getWidth(), c.getHitbox().getWidth(), 0.001);
 	}
-	
+	/**
+	 * Testing to see if the x-coordinate of the hitbox is accurate
+	 */
 	@Test
 	public void test_getHitboxX() {
 		Sprite c = new Sprite(10,20,30);
 		Rectangle hitbox = new Rectangle(15, 35, 10 , 10);
 		assertEquals("Testing Hitbox height" , hitbox.getX(), c.getHitbox().getX(), 0.001);
 	}
-	
+	/**
+	 * Testing to see if the y-coordinate of the hitbox is accurate
+	 */
 	@Test
 	public void test_getHitboxY() {
 		Sprite c = new Sprite(10,20,30);
