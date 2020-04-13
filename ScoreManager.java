7b5f2facc35fd2ccd1ac11dae395b742b07f4bfc
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-04-13 16:49:21 -0500

    Update ScoreManager.java

--- ScoreManager.java
+++ ScoreManager.java
@@ -4,6 +4,10 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class ScoreManager {
+	
+	/*
+	 * Instance variables for Score Manager class
+	 */
 	private int tenthsecondsPassed = 0;
 	private double obstacle = 1000;
 	/**
@@ -12,6 +16,10 @@ public class ScoreManager {
 	Timer scoreCalculator = new Timer();
 	TimerTask task = new TimerTask() {
 
+		/**
+		 * RUN METHOD
+		 * Increases the seconds passed variable.
+		 */
 		public void run() {
 			tenthsecondsPassed++;
 			obstacle = obstacle - 10;
@@ -20,35 +28,46 @@ public class ScoreManager {
 	};
 	
 	/**
-	 *  Increment score for every tenth of a second and print it
+	 * START METHOD
+	 *  Increments score for every tenth of a second
 	 */
 	public void start() {
 		scoreCalculator.scheduleAtFixedRate(task, 0, 10);
 	}
 	
 	/**
+	 *  SCORE LABEL METHOD
 	 *  Where the score label will be placed in the GUI version
-	 * @param score
+	 * @param score in int type.
 	 */
 	public void scoreLabel(int score){
 		Label scoreLabel = new Label();
 		scoreLabel.setFont(new Font("arial", Font.PLAIN, 14));
 	}
+	
 	/**
-	 *  Start of the score
-	 * @param args
+	 *  MAIN METHOD
+	 *  Starts the score
+	 * @param args, not used.
 	 */
 	public static void main(String[] args) {
 		ScoreManager currenttime = new ScoreManager();
 		currenttime.start();
 	}
+	
 	/**
-	 *  Returns the time in tenthsecondsPassed
+	 *  GETTER FOR TIME
+	 *  @return the time in tenthsecondsPassed
 	 */
 	public int gettime()
 	{
 	return  tenthsecondsPassed;
 	}
+	
+	/**
+	 * GETTER FOR OBSTACLE
+	 * @return returns obstacle variable used for obstacle timeline.
+	 */
 	public double getobstacle()
 	{
 		return obstacle;
