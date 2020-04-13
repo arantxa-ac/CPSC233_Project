03dd83f6f99a31bd19102d255014a233dcca56bc
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-04-13 17:27:34 -0500

    Update Sprite.java

--- Sprite.java
+++ Sprite.java
@@ -1,26 +1,38 @@
 import javafx.scene.shape.Rectangle;
 
 public class Sprite  {
-	private int hitboxSize;
-	/**
-	 * These x and y locations will be pixel locations on the GUI
+	/*
+	 * Instance variables for Sprite class.
 	 */
+	private int hitboxSize;
 	private Rectangle hitbox;
 	
-	//Constructor for the Sprite
+	/**
+	 * SPRITE CONSTRUCTOR
+	 * @param Side value for the hitbox in pixels using integer as the type.
+	 * @param X coordinate int for hitbox pixel location which is the same as the sprite location.
+	 * @param Y coordinate int for hitbox pixel location.
+	 */
 	public Sprite(int hitboxSize, int x, int y) {
 		this.hitboxSize = hitboxSize;
 		setHitbox(x,y);
 	}
 	
 	/**
+	 * GETTER FOR HITBOX
 	 * Generates a hitbox around each sprite for the player class to later check for collision detection. 
-	 * @return a rectangle hitbox around each sprite for collision detection
+	 * @return a rectangle hitbox around each sprite for collision detection.
 	 */
 	public Rectangle getHitbox() {
 		return hitbox;
 	}
 	
+	/**
+	 * SETTER FOR HITBOX
+	 * Creates a new rectangle in the location given by x and y coordinates.
+	 * @param newX coordinate for the hitbox.
+	 * @param newY coordinate for the hitbox.
+	 */
 	public void setHitbox(int newX, int newY) {
 		hitbox = new Rectangle(newX-hitboxSize/2, newY+hitboxSize/2, hitboxSize, hitboxSize);
 	}
