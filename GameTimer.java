c50dcd3c85ff00a5bfeab67599e4ee00010f2de0
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-04-13 14:47:53 -0500

    Update GameTimer.java

--- GameTimer.java
+++ GameTimer.java
@@ -4,9 +4,14 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class GameTimer {
+	/*
+	 * Instance variables for Game Timer.
+	 */
 	private int secondsPassed = 0;
 
-	// Add a timer for each second
+	/*
+	 * Add a timer for each second
+	 */
 	Timer timer = new Timer();
 	TimerTask task = new TimerTask() {
 		// Increment for each second that passed and print it to console
@@ -15,26 +20,39 @@ public class GameTimer {
 			System.out.println("Time: " + secondsPassed);
 		}
 	};
-	// Start the increment for each second that passes
+	
+	/**
+	 * START METHOD
+	 * Starts the increment for each second that passes.
+	 */
 	public void start() {
 		timer.scheduleAtFixedRate(task, 1000, 1000);
 	}
 	
-	// Where the label of the timer will be for the GUI version
+	/**
+	 * GAME TIMER LABEL METHOD
+	 * Where the label of the timer will be for the GUI version.
+	 */
 	public void gameTimerLabel() {
 		Label timerLabel = new Label();
 		timerLabel.setFont(new Font("arial", Font.PLAIN, 14));
 	}
 	
+	/**
+	 * GETTER FOR SECONDS PASSED
+	 * @return seconds passed since the start of the timer in int type.
+	 */
 	public int getTime()
 	{
 	return secondsPassed;
 	}
 	
-	// Start of the timer
+	/**
+	 * MAIN METHOD
+	 * Starts the timer.
+	 */
 	public static void main(String[] args) {
 		GameTimer currenttime = new GameTimer();
 		currenttime.start();
 	}
 }
-
