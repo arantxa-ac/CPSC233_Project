8d636ef6b876232cd6108b34b4ed2fc9c2b314c8
arantxa-ac <60748854+arantxa-ac@users.noreply.github.com>
2020-04-13 17:46:30 -0500

    Update TextGame.java

--- TextGame.java
+++ TextGame.java
@@ -1,16 +1,24 @@
 import java.util.ArrayList;
 
 public class TextGame {
+	
+	/*
+	 * Instance variables for Text Game class
+	 */
 	private static ArrayList<String> ground = new ArrayList<String>();
 	private static ArrayList<String> sky = new ArrayList<String>();
 	
+	/**
+	 * TEXT GAME CONSTRUCTOR
+	 * @param Array List of Game Objects to setup as the world in text version.
+	 */
 	public TextGame(ArrayList<GameObject> obj) {
-		//initialize a empty ground
+		//Initialize a empty ground
 		for (int i = 0; i< 18 ; i++) {
 			sky.add(" ");
 			ground.add(TextObstacle.getGround());
 		}
-			//Corresponding the position
+		//Corresponding the position
 		for (int i = 2; i< obj.size(); i++) {
 			int currentX = (int)(obj.get(i).getX()/40);
 			ground.remove(currentX);
@@ -18,7 +26,11 @@ public class TextGame {
 		}
 	
 	}
-		
+	
+	/**
+	 * TO PRINT METHOD
+	 * Prints the current world.		
+	 */
 	public void toPrint() {
 		//print out the arraylist
 		String thisground = "p";
@@ -31,7 +43,12 @@ public class TextGame {
 	    System.out.println(thisground);
 	}
 	
-	public static void generateType(boolean t) {
+	/**
+	 * GENERATE TYPE METHOD
+	 * Setup for the world if the obstacle generated is a bird.
+	 * @param Boolean with true if the obstacle generated is a bird or false otherwise.
+	 */
+	public void generateType(boolean t) {
 
 		if (t == false) {
 			ground.remove(17);
@@ -42,7 +59,11 @@ public class TextGame {
 		}
 	}
 	
-	public static void jump() {
+	/**
+	 * JUMP METHOD
+	 * Makes player jump by increasing its position in y.
+	 */
+	public void jump() {
 		String thisground = " ";
 		String thissky = "p";
 		for (int i = 0; i < ground.size(); i++) {
@@ -53,7 +74,11 @@ public class TextGame {
 	    System.out.println(thisground);
 	}
 	 
-	public static void updateByFrame() {
+	/**
+	 * UPDATE BY FRAME METHOD
+	 * Updates world.
+	 */
+	public void updateByFrame() {
 		for (int i = 0; i< 16; i++) {
 			sky.remove(i);
 			sky.add(i,sky.get(i+1));
