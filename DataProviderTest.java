c08b4327c723b8f771975c41de3b0f41ceb00950
hasan-na <60748908+hasan-na@users.noreply.github.com>
2020-04-13 18:24:12 -0400

    Update DataProviderTest.java

--- DataProviderTest.java
+++ DataProviderTest.java
@@ -2,9 +2,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
+/**
+ * 
+ * A Test class for the Data Provider class
+ *
+ */
 public class DataProviderTest {
-
+/**
+ * Testing to see if the getGROUND_SPEED method returns 2000 when called
+ */
 	@Test
 	public void test_getGROUND_SPEED() {
 		DataProvider d = new DataProvider();
@@ -12,7 +18,9 @@ public class DataProviderTest {
 		int actual = d.getGROUND_SPEED();
 		assertEquals("Testing Collision",  expected, actual);
 	}
-	
+	/**
+	 * Testing to see if the getBIRD_SPEED returns 450 when called
+	 */
 	@Test
 	public void test_getBIRD_SPEED() {
 		DataProvider d = new DataProvider();
@@ -20,7 +28,9 @@ public class DataProviderTest {
 		int actual = d.getBIRD_SPEED();
 		assertEquals("Testing Collision",  expected, actual);
 	}
-	
+	/**
+	 * Testing to see if the getWINDOW_WIDTH method returns 600 when called
+	 */
 	@Test
 	public void test_getWINDOW_WIDTH() {
 		DataProvider d = new DataProvider();
@@ -28,7 +38,9 @@ public class DataProviderTest {
 		int actual = d.getWINDOW_WIDTH();
 		assertEquals("Testing Collision",  expected, actual);
 	}
-	
+	/**
+	 * Testing to see if the getWINDOW_HEIGHT method returns 400 when called
+	 */
 	@Test
 	public void test_getWINDOW_HEIGHT() {
 		DataProvider d = new DataProvider();
@@ -39,3 +51,4 @@ public class DataProviderTest {
 	
 
 }
+
