a4730b52eb0cec7a1d846c159d5df58ecb4b1f65
hasan-na <60748908+hasan-na@users.noreply.github.com>
2020-04-13 20:17:55 -0400

    Update SpriteTest.java

--- SpriteTest.java
+++ SpriteTest.java
@@ -26,7 +26,7 @@ public class SpriteTest {
 	public void test_getHitboxWidth() {
 		Sprite c = new Sprite(10,20,30);
 		Rectangle hitbox = new Rectangle(15, 35, 10 , 10);
-		assertEquals("Testing Hitbox height" , hitbox.getWidth(), c.getHitbox().getWidth(), 0.001);
+		assertEquals("Testing Hitbox width" , hitbox.getWidth(), c.getHitbox().getWidth(), 0.001);
 	}
 	/**
 	 * Testing to see if the x-coordinate of the hitbox is accurate
@@ -35,7 +35,7 @@ public class SpriteTest {
 	public void test_getHitboxX() {
 		Sprite c = new Sprite(10,20,30);
 		Rectangle hitbox = new Rectangle(15, 35, 10 , 10);
-		assertEquals("Testing Hitbox height" , hitbox.getX(), c.getHitbox().getX(), 0.001);
+		assertEquals("Testing Hitbox x-coordinate" , hitbox.getX(), c.getHitbox().getX(), 0.001);
 	}
 	/**
 	 * Testing to see if the y-coordinate of the hitbox is accurate
@@ -44,6 +44,6 @@ public class SpriteTest {
 	public void test_getHitboxY() {
 		Sprite c = new Sprite(10,20,30);
 		Rectangle hitbox = new Rectangle(15, 35, 10 , 10);
-		assertEquals("Testing Hitbox height" , hitbox.getY(), c.getHitbox().getY(), 0.001);
+		assertEquals("Testing Hitbox y-coordinate" , hitbox.getY(), c.getHitbox().getY(), 0.001);
 	}
 }
